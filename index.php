dfc5546b574c68870cfa4c9e71faae22ad8b153c
JunaidiPamungkas <pamungkasjunaidi33@gmail.com>
2019-08-19 13:02:41 +0700

    menambahkan paragraph

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, sapiente molestias dolores minima delectus ea velit beatae tenetur officia tempore iste dicta ab distinctio magni consectetur, eius saepe doloribus libero?</p>
 </body>
 </html>
